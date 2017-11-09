64f4da854941d20215bef1bf7742042e95eace84
TIC <TIC@PCFORMACION015.ayesa.es>
2017-11-09 17:38:40 +0100

    subida inicial

---
+++ src/java/model/Saludador.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("saludador")
+@SessionScoped
+
+public class Saludador implements Serializable {
+
+    private String saludo;
+
+    public Saludador() {
+        saludo = "Hola caracola!";
+    }
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+
+}
